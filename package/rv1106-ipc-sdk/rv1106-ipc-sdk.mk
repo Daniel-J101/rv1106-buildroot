8c7a7493648cd90b52f0a8087bc15030f582bd80
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2025-06-27 18:17:57 -0500

    luckfox pico max support
    setup conversion to external buildroot format

---
+++ package/rv1106-ipc-sdk/rv1106-ipc-sdk.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# rv1106-ipc-sdk
+#
+################################################################################
+
+RV1106_IPC_SDK_SITE = https://github.com/LuckfoxTECH/luckfox-pico.git
+RV1106_IPC_SDK_SITE_METHOD = git
+RV1106_IPC_SDK_VERSION = e1caff31f64cbb1e7dbed793b764bbf1c020d6c7
+
+$(eval $(generic-package))
