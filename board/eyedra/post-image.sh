bfa20c88695346c02f230c13cea2719af6a724b1
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2025-06-27 18:23:05 -0500

    remove unnecessary support

--- board/eyedra/post-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mv -f $BINARIES_DIR/rootfs.ubi $BINARIES_DIR/rootfs.img 2>/dev/null
-mv -f $BINARIES_DIR/uboot-env.bin $BINARIES_DIR/env.img 2>/dev/null
-rm -f $BINARIES_DIR/*.dtb
-rm -f $BINARIES_DIR/rootfs.ubifs
